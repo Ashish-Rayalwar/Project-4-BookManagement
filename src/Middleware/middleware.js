a39c23b56ed14237c4f1ef1fd6d717582623269a
Chandrashekhar Singh Mourya <118621709+Singh-csm@users.noreply.github.com>
2023-01-29 22:41:45 +0530

    check-done

--- src/Middleware/middleware.js
+++ src/Middleware/middleware.js
@@ -8,23 +8,23 @@ const bookModel = require("../Models/bookModel");
 
 const verifyToken = async (req,res,next)=>{
    
-    let token = req.headers["x-api-key"]
+    let token = req.headers["x-api-key"];
 
-    if(!token) return res.status(400).send({status:false,msg:"Token is mandatory"})
+    if(!token) return res.status(400).send({status:false, message:"Token is mandatory"})
 
-    if(!validator.isJWT(token)) return res.status(400).send({status:false,msg:"Token is invalid"})
+    if(!validator.isJWT(token)) return res.status(400).send({status:false, message:"Token is invalid"})
 
     if(token){
 
     jwt.verify(token, "group2project-4",(err,tokenDetails)=>{
-        if(err) return res.status(403).send({status:false,msg:"Token is Invalid or expire"})
+        if(err) return res.status(403).send({status:false, message:"Token is Invalid or expire"})
 
        
         req.tokenDetails = tokenDetails
         next()
     })
     }else{
-        return res.status(401).send({status:false,msg:"you are not authenticated"})
+        return res.status(401).send({status:false, message:"you are not Authenticated Person to perform this Task!"})
     }
    
 }
@@ -33,14 +33,14 @@ const verifyToken = async (req,res,next)=>{
 
 const verifyTokenAndAuthorization = async(req,res,next)=>{
     verifyToken(req,res,async()=>{
-            if(!mongoose.isValidObjectId(req.params.bookId)) return res.status(400).send({status:false,message:"Invalid bookId"})
+            if(!mongoose.isValidObjectId(req.params.bookId)) return res.status(400).send({status:false, message:"Invalid bookId"})
             let book = await bookModel.findById(req.params.bookId)
-            if(!book) return res.status(404).send({status:false,message:"Book not found, plz check bookId"})
+            if(!book) return res.status(404).send({status:false, message:"Book not found, plz check bookId"})
             
         if(req.tokenDetails.userId === book.userId.toString()){
             next()
         }else{
-            res.status(403).send({msg:"you are not authorized to perform this task"})
+            res.status(403).send({message:"you are not authorized to perform this task"})
         }
     })
 }
