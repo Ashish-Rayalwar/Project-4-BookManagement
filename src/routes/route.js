d0828fddc697772e3ede66e9a2e8fb5ea6021415
Chandrashekhar Singh Mourya <118621709+Singh-csm@users.noreply.github.com>
2023-01-29 22:45:34 +0530

    check-done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const { registerUser,loginUser } = require("../Controller/userController")
-const { createBook,getBooks,getBookById,updateBookById,deleteBookById } = require("../Controller/bookController.js")
-const { verifyToken,verifyTokenAndAuthorization } = require("../Middleware/middleware")
-const { createReview,reviewUpdate,reviewDelete } = require("../Controller/reviewController")
+const { registerUser, loginUser } = require("../Controller/userController")
+const { createBook, getBooks, getBookById, updateBookById, deleteBookById } = require("../Controller/bookController.js")
+const { verifyToken, verifyTokenAndAuthorization } = require("../Middleware/middleware")
+const { createReview, reviewUpdate, reviewDelete } = require("../Controller/reviewController")
 
 
 const route = express.Router()
@@ -12,7 +12,7 @@ route.post("/register", registerUser )
 route.post("/login", loginUser )
 route.post("/books", verifyToken, createBook)
 route.get("/books", verifyToken, getBooks)
-route.get("/books/:bookId",  getBookById)
+route.get("/books/:bookId", getBookById)
 route.put("/books/:bookId", verifyTokenAndAuthorization, updateBookById)
 route.delete("/books/:bookId", verifyTokenAndAuthorization, deleteBookById)
 route.post("/books/:bookId/review", createReview)
@@ -23,8 +23,7 @@ route.delete("/books/:bookId/review/:reviewId", reviewDelete)
 
 
 route.all("/*",(req,res)=>{
-    console.log("Plz enter valid route");
-    res.status(400).send({msg:"invalid route"})
+    res.status(400).send({message:"invalid route"})
 })
 
 
