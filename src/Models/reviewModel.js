9e555d1cb532697313ed492f3580c6e20fe64af3
Chandrashekhar Singh Mourya <118621709+Singh-csm@users.noreply.github.com>
2023-01-29 22:48:17 +0530

    check-done

--- src/Models/reviewModel.js
+++ src/Models/reviewModel.js
@@ -2,23 +2,12 @@ const { default: mongoose } = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const reviewSchema = new mongoose.Schema(
   {
-    bookId: { type: ObjectId, required: true, ref: "Book" },
-    reviewedBy: {
-      type: String,
-      required: true,
-      trim:true,
-      default: "Guest"
-     
-    },
-
-    reviewedAt: { type: Date, required: true },
-
-    rating: { type: Number, required: true },
-    review: { type: String, trim:true },
-
-    isDeleted: { type: Boolean, default: false },
-  },
-  { timestamps: true }
-);
+      bookId: { type: ObjectId, required: true, ref: "Book" },
+      reviewedBy: { type: String, required: true, trim:true, default: "Guest"},
+      reviewedAt: { type: Date, required: true },
+      rating: { type: Number, required: true },
+      review: { type: String, trim:true },
+      isDeleted: { type: Boolean, default: false },
+  },{ timestamps: true });
 
 module.exports = mongoose.model("Review", reviewSchema);
