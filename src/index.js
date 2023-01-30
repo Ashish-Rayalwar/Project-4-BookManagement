af6a07a0153065aa3d8b95a1156e8b2150535d03
Ashish Rayalwar <rayalwar.ashish.1@gmail.com>
2023-01-30 12:17:34 +0530

    bookManagementProject

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
 const express = require("express")
 const  mongoose  = require("mongoose")
 mongoose.set('strictQuery', false);
-const dotenv = require("dotenv").config()
+const cors =require("cors")
 const route = require("./routes/route.js")
 
 
-const url = process.env.dbConnection
-const PORT = process.env.port || 3000
+
+const PORT =  3000
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
 
 app.use("/", route)
 
-const dbconnection = async (url)=>{
+const dbconnection = async ()=>{
    try {
-   await mongoose.connect(url,{useNewUrlParser:true})
+   await mongoose.connect("mongodb+srv://Ashish:7SiSkJ8Z0nkx2EWh@cluster0.8dgrxmt.mongodb.net/group2Database",{useNewUrlParser:true})
     console.log("Database connect");
    } catch (error) {
     console.log("error while db connection", error.message);
    }
 }
 
-dbconnection(url)
+dbconnection()
 
 app.listen(PORT ,()=>{
     console.log(`server start on port ${PORT}`);
